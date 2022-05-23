6c23e45d5f3953038e7a658551f70e5d0f357213
Etarley <standecristo@gmail.com>
2022-05-22 23:50:13 -0400

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const geocode = require('./utils/geocode');
 const { error } = require('console');
 
 //*Define port
-const port = 3000;
+const port = process.env.PORT || 3000;
 //*Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
